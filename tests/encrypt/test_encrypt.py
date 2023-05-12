4f6a7923e0b9d42b7e35dab249d1990313c5b668
luizfilipelgs <luizfilipelgs@gmail.com>
2023-05-12 14:28:52 -0300

    feat: add def test_encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
-
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(2, 'aaaaaa')
+    with pytest.raises(TypeError):
+        encrypt_message('aaaaaa', 'aaaaaa')
+    with pytest.raises(TypeError):
+        encrypt_message(2, 2)
+    assert encrypt_message('UFF', 2) == 'F_FU'
+    assert encrypt_message('RJ', 0) == 'JR'
+    assert encrypt_message('Petrobras', 5) == 'orteP_sarb'
