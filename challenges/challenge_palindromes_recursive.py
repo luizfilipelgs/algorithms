bc2f36fb9c80a283019db01658c84187038b0f59
luizfilipelgs <luizfilipelgs@gmail.com>
2023-05-12 15:04:04 -0300

    feat: add is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,15 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+
+    if low_index >= high_index:
+        return True
+
+    if len(word) == 0:
+        return True
+
+    if word[low_index] != word[high_index]:
+        return False
+
+    return is_palindrome_recursive(word, low_index+1, high_index-1)
+    
