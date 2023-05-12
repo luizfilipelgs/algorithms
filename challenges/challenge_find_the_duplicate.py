4e6110c34310d467ee509991aca3c0a0eae619eb
luizfilipelgs <luizfilipelgs@gmail.com>
2023-05-12 16:24:40 -0300

    feat: add def find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,16 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or not isinstance(nums, list):
+        return False
+    
+    seen_nums = set()
+    
+    for num in nums:
+        if not isinstance(num, int) or num <= 0:
+            return False
+        if num in seen_nums:
+            return num
+        seen_nums.add(num)
+    
+    return False
+
+
